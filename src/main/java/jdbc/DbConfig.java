b88e52fa8ddd0652b7e36598f5cc04014d223602
vahid <vahid.d.w@gmail.com>
2021-09-18 17:35:31 +0430

    delete some code not usable

--- src/main/java/jdbc/DbConfig.java
+++ src/main/java/jdbc/DbConfig.java
@@ -5,11 +5,11 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.*;
 
 public class DbConfig {
-    public static final String URL = "jdbc:mysql://localhost:3306/sql_hr";
+    public static final String URL = "jdbc:mysql://localhost:3306/school";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "";
 
-    public Connection dbConnection() throws SQLException {
+    public static Connection dbConnection() throws SQLException {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setURL(URL);
         dataSource.setUser(USERNAME);
